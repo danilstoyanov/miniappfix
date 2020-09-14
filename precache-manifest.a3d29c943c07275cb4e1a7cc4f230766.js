e61404e495710daac850d1653778d10735cefce4
Danil Stoyanov <d.stoyanov@corp.mail.ru>
2020-09-14 22:03:25 +0300

    Updates

---
+++ precache-manifest.a3d29c943c07275cb4e1a7cc4f230766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88bf5bc50304006482003009dcc54513",
+    "url": "/miniappfix/index.html"
+  },
+  {
+    "revision": "a0a283a4dc784121661e",
+    "url": "/miniappfix/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "a0a283a4dc784121661e",
+    "url": "/miniappfix/static/js/2.0eaecc66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/miniappfix/static/js/2.0eaecc66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cda27ed2b451f7e470e5",
+    "url": "/miniappfix/static/js/main.cf598a29.chunk.js"
+  },
+  {
+    "revision": "859ccfec69f1cd01f7cf",
+    "url": "/miniappfix/static/js/runtime-main.6bdc86ea.js"
+  }
+]);
